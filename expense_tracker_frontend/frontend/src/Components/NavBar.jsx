ea0e8d22e17c04a9f0d9efd1d0293520e02de06a
hyperTH23 <letshwititshenolo21@gmail.com>
2025-11-11 23:59:26 +0200

    food

--- expense_tracker_frontend/frontend/src/Components/NavBar.jsx
+++ expense_tracker_frontend/frontend/src/Components/NavBar.jsx
@@ -6,28 +6,36 @@ function Navbar(){
 
     return(
 
-        <nav>
+        <nav className='navbar navbar-expand-lg bgwhite shadow-sm py-3'>
         <div className = 'container'>
-            <Link classname = 'navbar-brand fw bold' to = '/'>ExpenseTracker</Link>
-            <button className = 'navbar-toggler' data-bs-toggle = 'collapse' data-bs-target='#navMenu'>
+            <Link classname = 'navbar-brand fw bold text-success' to = '/'>
+            <img
+            src= '/favicon.svg'
+            alt = 'ExpenseTracker Logo'
+            width = '30'
+            height = '30'
+            className = 'me-2'/>
+            ExpenseTracker
+            </Link>
+            <button 
+            className = 'navbar-toggler' 
+             type = 'button'
+             data-bs-t>
                 <span className = 'navbar-toggler-icon'></span>
             </button>
 
             <div className = 'collapse navbar-collapse' id= 'navMenu'>
-                <ul className = 'navbar-nav ms-auto'>
-                    <li className = 'nav-item'>
-                        <Link ClassName = 'nav-link' to='/login'>Login</Link>
-                    </li>
-                    <li classname = 'nav-item'>
-                       <Link className = 'nav-link' to= '/register'>Register</Link>
-                    </li>
-                </ul>
-
+              <div className ='ms-auto d-flex align-items-center'>
+                <Link to = '/login' className = 'btn btn-outline-success me-2 rounded-pill px-4'>
+                Log In
+                </Link>
+                <Link to ='/register' className = 'btn btn-success rounded-pill px-4'>
+                Sign Up
+                </Link>
+              </div>
             </div>
-            
         </div>
-
-        </nav>
+    </nav>
     );
 
 
--- expense_tracker_frontend/frontend/src/pages/WelcomePage.jsx
+++ expense_tracker_frontend/frontend/src/pages/WelcomePage.jsx
@@ -1,16 +1,17 @@
 
 import NavBar from '../Components/NavBar';
-import Footer from '/components/Footer';
+import Footer from '../Components/Footer';
 import HeroSection from '../Components/HeroSection';
 
 function WelcomePage() {
   return (
-    <div className="welcome-page">
-      <HeroSection />
-      {/*footer*/}
-      <Footer/>
-      {/*Navigation Bar*/}
+    <div className="welcome-page d-flex flex-column min-vh-100">
+  
       <NavBar/>
+      <main className='flex-grow-1'>
+           <HeroSection />
+      </main>
+      <Footer/>
     </div>
   );
 }
