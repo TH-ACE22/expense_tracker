acbe589b82fd7048674d59b2fe5f49f314b7ef81
hyperTH23 <letshwititshenolo21@gmail.com>
2025-11-11 22:43:10 +0200

    updating-node.js-version

--- expense_tracker_frontend/frontend/src/pages/WelcomePage.jsx
+++ expense_tracker_frontend/frontend/src/pages/WelcomePage.jsx
@@ -1,9 +1,16 @@
+
+import NavBar from '../Components/NavBar';
+import Footer from '/components/Footer';
 import HeroSection from '../Components/HeroSection';
 
 function WelcomePage() {
   return (
     <div className="welcome-page">
       <HeroSection />
+      {/*footer*/}
+      <Footer/>
+      {/*Navigation Bar*/}
+      <NavBar/>
     </div>
   );
 }
