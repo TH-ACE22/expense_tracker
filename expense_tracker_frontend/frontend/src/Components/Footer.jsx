c74c579714c073f69eef4a904eeb528e0012b453
hyperTH23 <letshwititshenolo21@gmail.com>
2025-11-13 22:29:31 +0200

    improved the look

--- expense_tracker_frontend/frontend/src/Components/Footer.jsx
+++ expense_tracker_frontend/frontend/src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import '../styles/components/_footer.css';
+import {Link} from 'react-router-dom';
 
 
 
@@ -6,8 +7,22 @@ function Footer(){
 
     return (
 
-        <footer classname = 'footer bg-dark text-light py-3 text-center mt-auto'>
+        <footer classname = 'footer mt-auti py-4 bg-white text-center border-top'>
            <div classname = 'container'>
+            <ul classname = 'list-inline mb-2'>
+                 <li className = 'list-inline-item'>
+                    <Link to='/about' className = 'text-secondary text-decoration-none'>
+                       About
+                    </Link>
+                 </li>
+                 <li className = 'list-inline-item mx-3'>*</li>
+                 <li  className= 'list-inline-item'
+                 >
+                    <Link  to = '/contact' className = 'text-secondary text-decoration-none'>
+                    Contact
+                    </Link>
+                 </li>
+            </ul>
             <p className = 'mb - 0'> &copy; {new Date().getFullYear()} ExpenseTracker.All rights reserved.</p>
            </div>
 
